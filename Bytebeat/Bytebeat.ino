a43db6b046abe8e91a89dd87d365a1deca30c609
Pangrus <pangrus@gmail.com>
2020-02-03 09:54:35 +0100

    Update Bytebeat.ino

--- Bytebeat/Bytebeat.ino
+++ Bytebeat/Bytebeat.ino
@@ -11,6 +11,19 @@ long t = 0;
 int output = 0; 
  
 void setup () { 
+   // Enable 64 MHz PLL and use as source for Timer1
+  PLLCSR = 1 << PCKE | 1 << PLLE;
+  // Set up Timer/Counter1 for PWM output
+  TIMSK = 0;                              // Timer interrupts OFF
+  TCCR1 = 1 << PWM1A | 2 << COM1A0 | 1 << CS10; // PWM A, clear on match, 1:1 prescale
+  GTCCR = 1 << PWM1B | 2 << COM1B0;       // PWM B, clear on match
+  OCR1A = 128;
+  OCR1B = 128;               // 50% duty at start
+  // Set up Timer/Counter0 for 8kHz interrupt to output samples.
+  TCCR0A = 3 << WGM00;                    // Fast PWM
+  TCCR0B = 1 << WGM02 | 2 << CS00;        // 1/8 prescale
+  TIMSK = 1 << OCIE0A;                    // Enable compare match
+  OCR0A = 124;                            // Divide by 1000 
   pinMode (outputPin, OUTPUT);
 }
 
